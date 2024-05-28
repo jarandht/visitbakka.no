0e3e431099c73736bd1191a0d502abe7dd9d89bf
jarandht <ost515132@gmail.com>
2024-05-28 23:12:39 +0200

    vissit bakka

---
+++ data/components/nav.php
@@ -0,0 +1,9 @@
+<nav>
+    <div>
+        <img src="/img/logo.webp" alt="">
+        <h1>Visit Bakka</h1>    
+    </div>
+    <div>
+        <a href="#parking"><div><p>Parking</p></div></a>
+    </div>
+</nav>
